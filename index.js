bc50ab2fce8494779874baedda4489386f1348b9
Mike Sywensky <michael.sywensky@pfizer.com>
2016-07-10 00:03:53 -0400

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var mongoose = require ("mongoose");
 var express = require('express');
 var app = express();
 var path = require('path');
-var urlSchema = require('urlSchema');
+var URL = require('url');
+var urlSchema = require('./urlSchema');
 
 var uristring = 
   process.env.MONGODB_URI || 
@@ -19,29 +20,56 @@ mongoose.connect(uristring, function (err, res) {
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/new/:u", function(req,res) {
-  var u = request.params.u;
+app.get("/new/*", function(req,res) {
+  var cur = req.originalUrl;
+  var i = cur.indexOf("/new/");
+  var u = cur.substring(i+5);
 
-  //valid url?
+  if (!validateUrl(u)) {
+    res.send({err:"invalid URL"});
+    return;
+  }
 
-  Save to db
+ urlSchema.saveUrl(u, function(o) {
+   console.log("in saveUrl callback");
+   var newurl = URL.parse(req.url).protocol + "//" + URL.parse(req.url).host;
+   var result = {original_url:o.url, short_url:newurl + "/" + o.idNum, parse:URL.parse(req.url)};
+    console.log("sending: " + result);
+    res.send(result);
+  })
 });
 
 app.get('/:id', function(req, res) {
-  var id = request.params.id;
+  var id = req.params.id;
 
   if (!isNumeric(id)) {
-    //send page warning bad url
+    res.send({err:"Invalid id"});
   }
 
-  //lookup id     
-  
+  urlSchema.getUrl(id, function(result) {
+    if (result.err) {
+      res.send(result);
+    } else {
+      res.redirect(result.url);
+    }
+
+  });
+
 
 });
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var validateUrl = function(textval) {
+    //console.log("testing " + textval);
+    var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    return urlregex.test(textval);
+}
 
 var isNumeric = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
--- urlSchema.js
+++ urlSchema.js
@@ -1,6 +1,45 @@
 var mongoose = require('mongoose');
 
-module.exports = new mongoose.Schema({
-	idNum:Number,
-	url: String,
-});
+var schema = new mongoose.Schema({
+		idNum:Number,
+		url: String,
+	});
+var model = mongoose.model('urls', schema);
+
+module.exports = {
+	saveUrl: function(url, callback) {
+		console.log("saveUrl is called " + url);
+		model.findOne()
+		.sort('-idNum')
+		.exec(function(err, member) {
+			var maxId = 1;
+			console.log("In exec");
+			if (err) {
+				console.log("Error trying to find record: " + err);
+			}
+			if (member != null) {
+				console.log("member is not null: " + member);
+				maxId = member.idNum + 1;
+			}
+
+			var newUrl = new model({idNum:maxId, url:url});
+			return newUrl.save().then(function(result) {
+				console.log("in then for save");
+				callback(result);
+				});
+		});
+	},
+	getUrl: function(id, callback) {
+		model.findOne({ 'idNum':id }, function(err, result) {
+			if (err) {
+				callback({err:"Unable to retrieve data: " + err});
+			}
+			if (result == null) {
+				callback({err:"Id " + id + " not found"});
+			} else {
+				callback({url: result.url});
+			}
+		});
+	}
+
+}
